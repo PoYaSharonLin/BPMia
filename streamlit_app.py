ceb2ef0085eb56207bf6143fd1302e6773d4f4fd
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-07-15 15:29:45 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,22 +69,19 @@ class OrchestratorAgent:
 
     def run(self):
         st.title(f"💬 On-boarding Mentor")
+        st.write("This is a website that could answers your queston about the department. ")
         
         # Create two columns: one for the text, one for the button
-        col1, col2 = st.columns([6, 1])  # Adjust the ratio as needed
+        col1, col2 = st.columns([5, 1])  # Adjust the ratio as needed
 
-        
         with col1:
-            st.write("This is a website that could answer your question about the department.")
+            st.write("Agent at this page could help you with general issues, such as drafting an email.")
 
-        
         with col2:
             if st.button("🔄 Restart Session"):
                 st.session_state.clear()
                 st.rerun()
 
-        st.write("This is a website that could answers your queston about the department. ")
-        st.write("Agent at this page could help you with general issues, such as drafting an email.")
         st.write("As for question answering with domain specific knowledge, please visit Chat with Notes!")
         UIHelper.setup_sidebar()
         chat_container = st.container()
