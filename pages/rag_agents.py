4509d17da661ccd920c2c0f2e2ae460d16971b1a
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-07-21 15:40:02 +0800

    Update rag_agents.py

--- pages/rag_agents.py
+++ pages/rag_agents.py
@@ -8,16 +8,6 @@ from typing import Dict, List  # type: ignore
 from utils.ui_helper import UIHelper
 from utils.llm_setup import LLMSetup   # type: ignore
 
-# Dialog to update USER_NAME
-if "show_dialog" not in st.session_state:
-    st.session_state.show_dialog = True
-
-if st.session_state.show_dialog:
-    with st.dialog("Enter Department Name"):
-        name_input = st.text_input("Department Name", Config.USER_NAME)
-        if st.button("Confirm"):
-            Config.USER_NAME = name_input
-            st.session_state.show_dialog = False
 
 class Config:
     """Configuration class for API keys and constants."""
@@ -25,6 +15,24 @@ class Config:
     
     # Default value
     USER_NAME = "On-Boarding Mentor"
+
+    # Show dialog trigger
+    if "show_dialog" not in st.session_state:
+        st.session_state.show_dialog = True
+    
+    # Define the dialog
+    @st.dialog("Enter Department Name")
+    def name_dialog():
+        name_input = st.text_input("Department Name", Config.USER_NAME)
+        if st.button("Confirm"):
+            Config.USER_NAME = name_input
+            st.session_state.show_dialog = False
+            st.rerun()
+    
+    # Show the dialog
+    if st.session_state.show_dialog:
+        name_dialog()
+    
     USER_IMAGE = "https://www.w3schools.com/howto/img_avatar.png"
     PLACEHOLDER = "Please input your command"
     SEED = 42
