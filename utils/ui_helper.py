74f0335313f664ad99e6b5441d182771114664c3
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-08 10:56:24 +0800

    Update ui_helper.py

--- utils/ui_helper.py
+++ utils/ui_helper.py
@@ -25,7 +25,7 @@ class UIHelper:
             # Group related pages under an expander
             with st.expander("Notes & Actions", expanded=True):
                 st.page_link("pages/documents_upload.py",
-                             label="Upload Notes", icon="📁")
+                             label="Manage Notes", icon="📁")
                 st.page_link("pages/loading_visualization.py",
                              label="Loading Mia", icon="📈")
 
