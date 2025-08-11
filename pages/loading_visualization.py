335266f24d64f0d337e15f99c8aba8d9fb38d46e
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-11 15:55:50 +0800

    Update loading_visualization.py

--- pages/loading_visualization.py
+++ pages/loading_visualization.py
@@ -211,7 +211,7 @@ def main():
                     group_labels = total_dram.iloc[0, 1:-1]  
                     values = total_dram.iloc[1, 1:-1].astype(float)
                     total_df = pd.DataFrame({'Group': group_labels, 'Value': values})
-                    collapsed = grouped_df.groupby('Group').sum().reset_index()
+                    collapsed = total_df.groupby('Group').sum().reset_index()
 
                     st.dataframe(collapsed)
 
