dcaf3febc261c7469d0faee9ca66e41c19278c84
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-12 15:36:14 +0800

    Update loading_visualization.py

--- pages/loading_visualization.py
+++ pages/loading_visualization.py
@@ -194,11 +194,11 @@ def main():
                     st.markdown("**Select a week range**")
                     date_table = plot_data_all.copy()
                     date_table = date_table.iloc[:3, :]
-                    # raw_date_row = date_table.iloc[3, :]
+                    raw_date_row = date_table.iloc[-1]
                     # week_row = raw_date_row.apply(convert_to_week_format)
                     # new_row = pd.Series(["3", "WeekFormat"] + week_row.tolist())
                     # date_table.loc[len(date_table)] = new_row
-                    st.dataframe(date_table)
+                    st.dataframe(raw_date_row)
                     
                 
             
