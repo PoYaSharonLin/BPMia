6272adadce07095e587007949fa449c892de78e2
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-12 16:38:04 +0800

    Update loading_visualization.py

--- pages/loading_visualization.py
+++ pages/loading_visualization.py
@@ -219,13 +219,13 @@ def main():
                         st.dataframe(filtered, use_container_width=True)
                         quarter_map = filtered.iloc[1, 1:].tolist()
                         process_series = filtered.iloc[4:, 0].reset_index(drop=True)
-                        # values = filtered.iloc[5:, 3:24].reset_index(drop=True)
+                        values = filtered.iloc[4:, 1:].reset_index(drop=True)
                         # values.columns = quarter_map
                         # values.insert(0, 'process_series', process_series)
                         
                         st.write(quarter_map)
                         st.write(process_series)
-                        # st.write(values)
+                        st.dataframe(values)
                         
 
             except Exception as e:
