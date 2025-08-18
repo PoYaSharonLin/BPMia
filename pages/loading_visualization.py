b90e7017df49ba80859c1a6da14f3086e5d4a743
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-18 13:16:34 +0800

    Update loading_visualization.py

--- pages/loading_visualization.py
+++ pages/loading_visualization.py
@@ -198,9 +198,9 @@ def main():
                 process_series_value = plot_data_all.iloc[3:11]
                 quarter_df = quarter.to_frame().T
                 portion_table = pd.concat([quarter_df, process_series_value], axis=0)
-                # bar_portion_table = portion_table[1:8, :]
+                bar_portion_table = portion_table[3:10, :]
                 st.dataframe(portion_table)
-                # st.dataframe(bar_portion_table)
+                st.dataframe(bar_portion_table)
                 
     
                 
