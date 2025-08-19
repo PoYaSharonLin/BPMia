e33011984b329fb8e1e7917df231dd6c0f91e480
PoYaSharonLin <sharon.lin@iss.nthu.edu.tw>
2025-08-19 15:31:34 +0800

    Update loading_visualization.py

--- pages/loading_visualization.py
+++ pages/loading_visualization.py
@@ -136,9 +136,9 @@ def main():
         uploaded_file = st.file_uploader("Upload your Excel file", type=["xlsx"])
         col1, col2 = st.columns(2)
         with col1:
-            start_cell = st.text_input("Enter start cell (e.g., CR44):", value="CR44")
+            start_cell = st.text_input("Enter start cell (e.g., CR46):", value="CR44")
         with col2:
-            end_cell = st.text_input("Enter end cell (e.g., JE58):", value="JE58")
+            end_cell = st.text_input("Enter end cell (e.g., JE60):", value="JE59")
 
         if uploaded_file:
             try:
